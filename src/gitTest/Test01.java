bfccbc593ddf22b7a36bced39b18ae83434df6d8
taehun kang <xogns991124@naver.com>
2021-11-13 11:14:38 +0900

    클래스도 추가해봤어요!

--- src/gitTest/Test01.java
+++ src/gitTest/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("두번쨰 커밋");
+		System.out.println("안녕하세용");
 	}
 
 }
---
+++ src/gitTest/Test02.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		System.out.println("짜잔");
+	}
+
+}
